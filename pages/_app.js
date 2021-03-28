6e4468ef6fd99ee951e503adafdb98c8f9a6b1c2
Lulololu <73346696+Lulololu@users.noreply.github.com>
2021-03-28 19:21:54 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles/normalize.css"; // Next.js authorizes Materialize.css (Global CSS) to be imported ONLY from _app.js
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
 import Layout from "../Components/Layout";
 
 function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
